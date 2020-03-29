6e14dd4efeebec8c1173ce7314bac502dc91278e
NataSh <N.Shepina@bk.ru>
2020-03-29 15:26:30 +0300

    add GetAllInventory to InventoryDal

--- AutoLotDal/DataOperations/InventoryDal.cs
+++ AutoLotDal/DataOperations/InventoryDal.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
-using  System.Data.SqlClient;
+using System.Data.SqlClient;
+using AutoLotDal.Models;
 
 namespace AutoLotDal.DataOperations
 {
@@ -26,5 +28,30 @@ namespace AutoLotDal.DataOperations
         }
 
         private void CloseConnection() => _sqlConnection.Close();
+
+        public List<Car> GetAllInventory()
+        {
+            OpenConnection();
+            List<Car> inventory = new List<Car>();
+            string sql = "Select * from Inventory";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dataReader.Read())
+                {
+                    inventory.Add(new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color= (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["PetName"]
+                    });
+                }
+                dataReader.Close();
+            }
+
+            return inventory;
+        }
     }
 }
