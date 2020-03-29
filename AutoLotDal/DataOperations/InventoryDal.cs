f60b4e1fa1058c2f3a2b61a27f299e85b9b37ca4
NataSh <N.Shepina@bk.ru>
2020-03-29 15:15:10 +0300

    Add Car.cs

--- AutoLotDal/DataOperations/InventoryDal.cs
+++ AutoLotDal/DataOperations/InventoryDal.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Data;
+using  System.Data.SqlClient;
+
 namespace AutoLotDal.DataOperations
 {
     public class InventoryDal
     {
-        
+        private readonly string _connectionString;
+        private SqlConnection _sqlConnection = null;
+
+        public InventoryDal() :
+            this(@"СТРОКА ПОДКЛЮЧЕНИЯ")
+        {
+        }
+
+        public InventoryDal(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private void OpenConnection()
+        {
+            _sqlConnection=new SqlConnection(_connectionString);
+            _sqlConnection.Open();
+        }
+
+        private void CloseConnection() => _sqlConnection.Close();
     }
 }
